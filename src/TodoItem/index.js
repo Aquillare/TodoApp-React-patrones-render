0bd9ee0a16da18123193c6c091e027fa75a06a83
Miguel <migueleduagui@gmail.com>
2022-12-08 12:03:09 -0300

    fix desbordamiento palabras

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -11,22 +11,36 @@ const TodoItem = (props) => {
 
     return(
         <li className='todo_item-li'>
-            <span className={(completed && 'todo_check') || (`${!completed  && 'todo_checkOff' }` )}
-            onClick={onComplete}>
-                <img src={checkIcon} alt="check Icon"/>
-            </span>
+            
+            <button className='todo_action_button'>
+                <span className={(completed && 'todo_check') || (`${!completed  && 'todo_checkOff' }` )}
+                onClick={onComplete}>
+                    <img src={checkIcon} alt="check Icon"/>
+                </span>
+            </button>
+            
 
             <p className={(!completed && 'todo_item-text') || (`${completed && 'todo_item-text2' }`)}>{props.text}</p>
 
-            <span className='todo_edit'
-            onClick={onOpenModal}>
-                <img src={editIcon} alt="edit Icon"/>
-            </span>
+            <div className='todo_buttons_container'>
 
-            <span className='todo_delete'
-            onClick={onDelete}>
-                <img src={deleteIcon} alt="delete Icon"/>
-            </span>
+                <button className='todo_action_button'>
+                    <span className='todo_delete_span'
+                    onClick={onDelete}>
+                        <img src={deleteIcon} alt="delete Icon"/>
+                    </span>
+                </button>
+
+                <button className='todo_action_button'>
+                    <span className='todo_edit'
+                    onClick={onOpenModal}>
+                        <img src={editIcon} alt="edit Icon"/>
+                    </span>
+                </button>
+
+               
+            </div>
+            
         </li>
     );
 }
